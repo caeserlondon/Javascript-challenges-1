e2d479f80dd2efc0a215922d197b01a271af3f3f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-10 11:40:39 +0700

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-function reverseNumber(Int) {}
+// challenge 1 ..  reverse a string .
+//return a string in reverseNumber.
+function reverseNumber(Int) {
+	str;
+}
+{
+	return "the solution is ";
+}
+
+//  call function
+const output = reverseString("hello");
+
+console.log(output);
