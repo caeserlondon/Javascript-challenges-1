e4fb3f3dc4ec9e3cfb3db237d65e7714b240e4ed
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-10 13:21:25 +0700

    challenge 2 solution

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
 // challenge 1 ..  reverse a string .
-//return a string in reverseNumber.
-function reverseString(str) {
-	//	return str.split("").reverse().join("");
+// //return a string in reverseNumber.
+// function reverseString(str) {
+//	return str.split("").reverse().join("");
 
-	//////////////////////
+//////////////////////
 
-	// let revString = "";
-	// for (let i = str.lenght - 1; i >= 0; i--) {
-	// 	//	revString = revString + str[i];
-	// 	console.log(i);
-	// }
-	// return revString;
+// let revString = "";
+// for (let i = str.lenght - 1; i >= 0; i--) {
+// 	//	revString = revString + str[i];
+// 	console.log(i);
+// }
+// return revString;
 
-	///////////////////
+///////////////////
 
-	// let revString = "";
-	// for (let i = 0; i <= str.length - 1; i++) {
-	// 	revString = str[i] + revString;
-	// }
-	// return revString;
+// let revString = "";
+// for (let i = 0; i <= str.length - 1; i++) {
+// 	revString = str[i] + revString;
+// }
+// return revString;
 
-	////////////////////////
+////////////////////////
 
-	// let revString = "";
-	// for (let char of str) {
-	// 	revString = char + revString;
-	// }
-	// return revString;
+// let revString = "";
+// for (let char of str) {
+// 	revString = char + revString;
+// }
+// return revString;
 
-	// //////////////////////
+// //////////////////////
 
-	// let revString = "";
-	// str.split("").forEach((char) => {
-	// 	revString = char + revString;
-	// });
-	// return revString;
+// let revString = "";
+// str.split("").forEach((char) => {
+// 	revString = char + revString;
+// });
+// return revString;
 
-	//////////////////////////////
+//////////////////////////////
 
-	return str.split("").reduce((revString, char) => char + revString, "");
+// 	return str.split("").reduce((revString, char) => char + revString, "");
+
+// const output = reverseString("hello");
+// }
+// challenge 2   validate a palindrome and return true or false.
+
+function isPalindrome(str) {
+	const revString = str.split("").reverse().join("");
+	return revString === str;
 }
 
 //  call function
-const output = reverseString("hello");
+const output = isPalindrome("madam");
 
 console.log(output);
